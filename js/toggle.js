8cc70689a0fd371a2d8ab4cd6a9ed0c80d71a8b1
Elias-ntk <eliassegovia43@gmail.com>
2022-12-07 16:49:16 -0300

    Se agraga el proyecto

---
+++ js/toggle.js
@@ -0,0 +1,19 @@
+let toggle = document.getElementById('container-tgl');
+let body = document.querySelector('body');
+
+toggle.addEventListener('click', ()=>{
+    toggle.classList.toggle('active');
+    let val = body.classList.toggle('active');
+    localStorage.setItem("container-tgl", val );
+});
+
+let valor = localStorage.getItem("container-tgl");
+
+
+if (valor == "true"){
+    body.classList.add("active");
+    toggle.classList.add('active');
+} else {
+    body.classList.remove("active");
+    toggle.classList.remove('active');
+}
